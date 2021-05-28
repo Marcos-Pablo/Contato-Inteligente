7e644dae34b3bd62d2934322ece13395af73318a
Marcos Pablo Souza de Almeida <marquin.pablo@outlook.com>
2021-05-28 02:11:39 -0300

    New - Requisição para busca dos 5 repositórios mais antigos de c#

--- Contato-Inteligente/Api/Controllers/GitHubRepositoriesController.cs
+++ Contato-Inteligente/Api/Controllers/GitHubRepositoriesController.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
-    public class GitHubRepositoriesController
+    [ApiController]
+    [Route("/repositories")]
+    public class GitHubRepositoriesController : ControllerBase
     {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetFiveOlderCSharpRepositories()
+        {
+            try
+            {
+                var productInformation = new ProductHeaderValue("Marcos-Pablo");
+                var credentials = new Credentials("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");
+                var client = new GitHubClient(productInformation) { Credentials = credentials };
+
+                var takeRepositories = await client.Repository.GetAllForUser("takenet");
+                var fiveOlderCSharpRepositories = takeRepositories
+                                                    .Where(repo => repo.Language == "C#")
+                                                    .OrderBy(repo => repo.CreatedAt)
+                                                    .Take(5);
+
+                return Ok(fiveOlderCSharpRepositories);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro interno de servidor");
+            }
+        }
     }
 }
